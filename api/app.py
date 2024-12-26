865a32a6d2ec1e9f4d411d777c7157d945cc669f
Artem Panov <arsepanXXX@mail.ru>
2024-12-26 19:21:09 +0300

    Add handle predict

--- api/app.py
+++ api/app.py
@@ -8,7 +8,6 @@ import pandas as pd
 from pydantic import BaseModel
 import pickle
 from catboost import CatBoostRegressor
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 from preprocess import *
 from typing import Dict, Any
@@ -51,7 +50,6 @@ async def upload_dataframe(request: Request):
             raise ValueError("Данные не являются объектом DataFrame")
 
         df = dataframe
-        print(type(df))
 
         logger.info("DataFrame успешно получен")
         return {"message": "DataFrame успешно получен"}
@@ -103,33 +101,45 @@ async def get_profile():
     except Exception as e:
         logger.error(f"Ошибка при создании профиля: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Ошибка при создании профиля: {str(e)}")
-    
+
 
 @app.post("/train_model")
 async def train_model(request: TrainModelRequest):
     global df
     global models
 
+    logger.info("Вызов /train_model")
     if df is None or df.empty:
         raise HTTPException(status_code=404, detail="DataFrame is empty or not initialized")
 
     try:
         data = preprocess_data(df)
+        logger.info("Обработка DF прошла успешно")
     except Exception as e:
+        logger.info("Обработка DF прошла неуспешно")
         raise HTTPException(status_code=500, detail=f"Error during preprocessing: {str(e)}")
 
     try:
-        X_train, X_test, y_train, y_test = preprocess_data_for_model(data)
+        X_train, X_val, X_test, y_train, y_val, y_test = preprocess_data_for_model(data, is_trained = True)
+        logger.info("Разбиение на train/val/test прошло успешно")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during preprocessing data for model: {str(e)}")
 
     try:
+        logger.info("Начало обучение модели")
         model = CatBoostRegressor(**request.hyperparameters)
+
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Invalid hyperparameters: {str(e)}")
 
     try:
-        model.fit(X_train, y_train, eval_set=(X_test, y_test), verbose=True)
+        model.fit(
+            X_train, 
+            y_train, 
+            eval_set=(X_val, y_val), 
+            verbose=True, 
+            plot=False
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during model training: {str(e)}")
 
@@ -145,13 +155,20 @@ async def train_model(request: TrainModelRequest):
         "name": request.model_name,
         "model": model,
         "status": "trained",
+        "hyperparameters": request.hyperparameters,
         "metrics": {
             "RMSE": rmse,
             "R2": r2
+        },
+        "learning_curves": {
+            "iterations": list(range(len(model.evals_result_['validation']['RMSE']))),
+            "train_rmse": model.evals_result_['learn']['RMSE'],
+            "test_rmse": model.evals_result_['validation']['RMSE']
         }
     }
 
     models[request.model_id] = model_info
+
     response = {
         "id": model_info["id"],
         "name": model_info["name"],
@@ -160,8 +177,36 @@ async def train_model(request: TrainModelRequest):
     }
     return response
 
+
+@app.get("/get_model_info/{model_id}")
+async def get_model_info(model_id: str):
+    logger.info("Вызов /get_model_info")
+    if model_id not in models:
+        raise HTTPException(status_code=404, detail=f"Model with id {model_id} not found")
+    
+    model_info = models[model_id]
+    return {
+        "model_id": model_id,
+        "model_name": model_info["name"],
+        "hyperparameters": model_info["model"].get_params(),
+        "metrics": model_info["metrics"]
+    }
+
+@app.get("/get_learning_curves/{model_id}")
+async def get_learning_curves(model_id: str):
+    logger.info("Вызов /get_learning_curves")
+    if model_id not in models:
+        raise HTTPException(status_code=404, detail=f"Model with id {model_id} not found")
+    
+    model_info = models[model_id]
+    
+    return {
+        'learning_curves': model_info["learning_curves"]
+    }
+
 @app.delete("/models/{model_id}")
 async def delete_model(model_id: str):
+    logger.info(f"Удаление модели {model_id}")
     global models
     if model_id in models:
         del models[model_id]
@@ -171,6 +216,26 @@ async def delete_model(model_id: str):
 
 @app.delete("/models")
 async def delete_all_models():
+    logger.info("Удаление всех моделей")
     global models
     models.clear()
     return {"message": "All models have been deleted."}
+
+@app.post("/predict/{model_id}")
+async def predict(model_id: str, request: Request):
+    logger.info("Инференс модели")
+    if model_id not in models:
+        raise HTTPException(status_code=404, detail=f"Модель с ID '{model_id}' не найдена.")
+    try:
+        dataframe_bytes = await request.body()
+        input_data = pickle.loads(dataframe_bytes)
+        if not isinstance(input_data, pd.DataFrame):
+            raise ValueError("Переданный объект не является DataFrame")
+        model_info = models[model_id]
+        model = model_info["model"]
+        processed_data = preprocess_data(input_data)
+        X = preprocess_data_for_model(processed_data, is_trained=False)
+        predictions = model.predict(X)
+        return {"predictions": predictions.tolist(), "model_id": model_id}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Ошибка обработки данных или выполнения инференса: {str(e)}")
