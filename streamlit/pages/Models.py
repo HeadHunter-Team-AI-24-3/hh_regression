d9cc945b59ced15c8858ef7341063441ffcf4556
Liza Bolbot <bolbot.elizaveta2002@gmail.com>
2024-12-29 20:04:36 +0300

    add_streamlit_pages

--- streamlit/pages/Models.py
+++ streamlit/pages/Models.py
@@ -1,10 +1,104 @@
 import streamlit as st
+import json
+import requests
+from pages.Dataset import FASTAPI_HOST
 
-st.set_page_config(
-    page_title="Модели",   # Название в меню
-    page_icon="",           # Иконка в меню
-    layout="wide"
-)
+st.set_page_config(page_title="Модель", page_icon="", layout="wide")
 
-st.title("Models Page")
-st.write("Страница для создания и редактирования моделей")
+st.title("Страница модели")
+tab1, tab2, tab3 = st.tabs(["✏️ Создание", "📁 Информация", "❌ Удаление"])
+
+
+def train_model(model_id, model_name, hyperparameters):
+    param = {
+        "model_id": model_id,
+        "model_name": model_name,
+        "hyperparameters": hyperparameters,
+    }
+    response = requests.post(f"{FASTAPI_HOST}/train_model", json=param)
+    if response.status_code == 200:
+        st.success(f"Модель {model_name} успешно обучена!")
+        st.json(response.json())
+    else:
+        st.error(f"Ошибка: {response.status_code}, {response.text}")
+
+
+def get_model_info(model_id):
+    response = requests.get(f"{FASTAPI_HOST}/get_model_info/{model_id}")
+    if response.status_code == 200:
+        st.json(response.json())
+    else:
+        st.error(f"Ошибка: {response.status_code}, {response.text}")
+
+
+def list_all_models():
+    response = requests.get(f"{FASTAPI_HOST}/get_models_info")
+    if response.status_code == 200:
+        models = response.json()
+        st.subheader("Список всех моделей:")
+        for model in models:
+            st.json(model)
+            st.write("---")
+    else:
+        st.error(f"Ошибка: {response.status_code}, {response.text}")
+
+
+def delete_model(model_id):
+    response = requests.delete(f"{FASTAPI_HOST}/models/{model_id}")
+    if response.status_code == 200:
+        st.success(f"Модель {model_id} успешно удалена!")
+    else:
+        st.error(f"Ошибка: {response.status_code}, {response.text}")
+
+
+def delete_all_models():
+    response = requests.delete(f"{FASTAPI_HOST}/models")
+    if response.status_code == 200:
+        st.success("Все модели успешно удалены!")
+    else:
+        st.error(f"Ошибка: {response.status_code}, {response.text}")
+
+
+with tab1:
+    st.write("Создание и редактирования моделей")
+    container = st.container(border=True)
+    model_id = container.text_input("ID модели", key=1)
+    model_name = container.text_input("Название модели", key=2)
+    hyperparameters = container.text_area(
+        "Гиперпараметры", value='{"iterations": 100, "learning_rate": 0.1, "depth": 6}'
+    )
+    if container.button("Обучить"):
+        train_model(model_id, model_name, json.loads(hyperparameters))
+
+with tab2:
+    st.write("Просмотр информации о модели")
+    option = st.selectbox(
+        "Информацию о скольки моделях хотите посмотреть?",
+        ("Об одной", "О всех"),
+        index=None,
+        placeholder="Select",
+    )
+
+    if option == "Об одной":
+        model_id_inf = st.text_input("ID модели", key=3)
+        if st.button("Просмотр информации"):
+            get_model_info(model_id_inf)
+    elif option == "О всех":
+        if st.button("Просмотр"):
+            list_all_models()
+
+with tab3:
+    option = st.selectbox(
+        "Сколько моделей хотите удалить?",
+        ("Одну", "Все"),
+        index=None,
+        placeholder="Select",
+    )
+
+    if option == "Одну":
+        model_id = st.text_input("Model ID")
+        if st.button("Удалить модель"):
+            delete_model(model_id)
+    elif option == "Все":
+        if st.button("Удалить все модели"):
+            delete_all_models()
---
+++ streamlit/pages/Prediction.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import requests
+import pickle
+from pages.Dataset import FASTAPI_HOST
+
+st.set_page_config(
+    page_title="Предсказать",
+    page_icon="",
+    layout="wide",
+)
+st.title("Страница для выполнения предсказаний")
+st.write(
+    "На этой странице можно выполнять предсказание с использованием обученных моделей"
+)
+
+
+def predict(model_id, input_data):
+    df = pd.read_csv(input_data)
+    df_serialized = pickle.dumps(df)
+    response = requests.post(f"{FASTAPI_HOST}/predict/{model_id}", data=df_serialized)
+    if response.status_code == 200:
+        predictions = response.json().get("predictions", [])
+        st.success(f"Предсказания для модели {model_id} выполнены успешно")
+        st.write(f"Предсказания: ")
+        st.write(predictions)
+    else:
+        st.error(f"Ошибка: {response.status_code}, {response.text}")
+
+
+container = st.container(border=True)
+model_id = container.text_input("ID модели")
+uploaded_file = container.file_uploader(
+    "Загрузите CSV файл для предсказания", type=["csv"]
+)
+if container.button("Выполнить предсказание"):
+    if not model_id:
+        container.error("Укажите ID модели")
+    elif not uploaded_file:
+        container.error("Загрузите файл с данными")
+    else:
+        predict(model_id, uploaded_file)
